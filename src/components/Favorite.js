54128afafffb07aa3b24c87d6eea883b9292e45f
Arfat Salman <giney.paradise@gmail.com>
2021-08-18 22:35:00 +0530

    change version of material-ui-lab

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -1,12 +1,23 @@
+import { useState } from 'react';
+
 function Favorite() {
-  const favs = ['bulb', 'char'];
-  return (
+  const [favs, setFavs] = useState(function () {
+    const value = localStorage.getItem('pokemons');
+    if (value) {
+      return JSON.parse(value);
+    }
+    return [];
+  });
+
+  return favs.length ? (
     <div>
+      <h3>Your Favorite Pokemons</h3>
+
       {favs.map((pokemon) => {
         return <div>{pokemon}</div>;
       })}
     </div>
-  );
+  ) : null;
 }
 
 export { Favorite };
